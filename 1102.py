d68e8df293562ba23c5c6419342f7dae162ec7c7
frogboy69 <xup61i6u6@gmail.com>
2022-11-11 15:35:23 +0800

    finish contour label format and 1102

---
+++ 1102.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import argparse
+
+import cv2
+import numpy as np
+
+
+def main():
+    args = get_args_parse().parse_args()
+    source: Path = args.source
+    img = cv2.imread(str(source))
+    mask = np.zeros_like(img)
+    contours = np.array([[248, 183],
+                         [201, 160],
+                         [120, 322],
+                         [167, 346]
+                         ])
+    x_vector = contours[1][0] - contours[2][0]
+    y_vector = contours[1][1] - contours[2][1]
+    contours[0][0] = contours[0][0] - x_vector * 0.2
+    contours[0][1] = contours[0][1] - y_vector * 0.2
+    contours[1][0] = contours[1][0] - x_vector * 0.2
+    contours[1][1] = contours[1][1] - y_vector * 0.2
+    contours[2][0] = contours[2][0] + x_vector * 0.3
+    contours[2][1] = contours[2][1] + y_vector * 0.3
+    contours[3][0] = contours[3][0] + x_vector * 0.3
+    contours[3][1] = contours[3][1] + y_vector * 0.3
+    cv2.drawContours(mask, [contours], -1, (255, 255, 255), -1)
+    mixed_img = cv2.addWeighted(img, 0.7, mask, 0.3, 0)
+    cv2.imshow("img", mixed_img)
+    cv2.waitKey()
+
+
+def get_args_parse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=Path)
+    return parser
+
+
+if __name__ == '__main__':
+    main()
